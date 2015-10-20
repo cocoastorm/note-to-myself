e190f61982dd96ea51aa34244941ee6655278740
Khoa Nguyen <khoa.tan.nguyen.96@gmail.com>
2015-10-20 15:56:00 -0700

    added captcha into config.php and composer

--- config/app.php
+++ config/app.php
@@ -137,6 +137,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        LaravelCaptcha\Providers\LaravelCaptchaServiceProvider::class,
 
         /*
          * Application Service Providers...
